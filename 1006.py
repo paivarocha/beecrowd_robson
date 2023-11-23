9de695116c8b3659dfd0540427d01e6544b38056
paivarocha <129407758+paivarocha@users.noreply.github.com>
2023-11-23 13:16:57 -0300

    Add files via upload

---
+++ 1006.py
@@ -0,0 +1,9 @@
+#Ler duas notas float
+n1 = float(input('Digite a primeira nota:'))
+n2 = float(input('Digite a segunda nota:'))
+
+#Realizar media
+media = (n1 * 3.5 + n2 * 7.5) / 11
+
+#Imprimir media format .5
+print(f'MEDIA = {media:.5f}')
---
+++ 1007.py
@@ -0,0 +1,10 @@
+#Ler tres valores int
+A = float (input('nota 1:'))
+B = float (input('nota 2:'))
+C = float (input('nota 3:'))
+
+#Calcular media A *2 B*3 C*3
+media = (A * 2 + B * 3 + C * 5) / 10
+
+#Printar media do aluno
+print(f'MEDIA = {media:.1f}')
